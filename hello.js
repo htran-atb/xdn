7f07956efcb1f74450d4d4cdc46a7b39ce38b406
Hai Tran <htran@atb.com>
2026-03-02 13:48:57 -0700

    Add hello v3, and distzip v3

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
-// version 3
+// version 4
 (function hello() {
   console.info("hello.js on github repo, delivered through jsDelivr");
-  console.info("Version 3");
+  console.info("Version 4");
 })();
